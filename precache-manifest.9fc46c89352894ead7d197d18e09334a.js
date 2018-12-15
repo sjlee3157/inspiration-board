09b350dd88f6d6950efc50e5b66babcbb23baa76
sjlee3157 <sjlee3157@gmail.com>
2018-12-14 20:56:12 -0800

    Updates

---
+++ precache-manifest.9fc46c89352894ead7d197d18e09334a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b537e5968c342373999",
+    "url": "/inspiration-board/static/css/main.95bd85b2.chunk.css"
+  },
+  {
+    "revision": "2b537e5968c342373999",
+    "url": "/inspiration-board/static/js/main.2b537e59.chunk.js"
+  },
+  {
+    "revision": "8ad4718fa3af1dcea190",
+    "url": "/inspiration-board/static/js/1.8ad4718f.chunk.js"
+  },
+  {
+    "revision": "d769499afd0cba5bf1c2",
+    "url": "/inspiration-board/static/js/runtime~main.d769499a.js"
+  },
+  {
+    "revision": "2188d7993f70c2d39a548fd3349aa460",
+    "url": "/inspiration-board/index.html"
+  }
+];
